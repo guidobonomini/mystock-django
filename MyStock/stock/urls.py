7b1f65327d400319f3caa910d4aa8b9d72965675
Guido <bono_9475@hotmail.com>
2016-07-16 17:34:35 -0300

    Update urls.py

--- MyStock/stock/urls.py
+++ MyStock/stock/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'home/$', views.index, name='index'),
-    url(r'searchYTS/(?P<query>\w+)/(?P<currentPage>[0-9]+)/$', views.searchYTS, name="searchYTS"),
-    url(r'searchKickass/(?P<query>\w+)/(?P<currentPage>[0-9]+)/$', views.searchKickass, name="searchKickass"),
+    url(r'stockPrices/$', views.index, name='index'),
 ]
