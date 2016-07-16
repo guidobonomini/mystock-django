a3dbef945152ca8cb008d38e6d44836463b6d085
Guido <bono_9475@hotmail.com>
2016-07-16 17:34:17 -0300

    Update views.py

--- MyStock/stock/views.py
+++ MyStock/stock/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
-from homepage.searchYTS import *
-from homepage.searchKickass import *
+from stock.services import get_stock
 
 def index(request):
-    return render(request, 'home.html')
-
-def searchYTS(request, query, currentPage):
     
-    search = SearchYTS()
-    result = search.searchYTS(query, currentPage)
-    return render(request, 'search.html', {'movies': result[1], 'pages': [0 for x in range(result[0])]})
-
-
-def searchKickass(request, query, currentPage):
-    
-    search = SearchKickass()
-    result = search.searchKickass(query, currentPage)
-    return render(request, 'search.html', {'movies': result[1], 'pages': [0 for x in range(result[0])]})
+    stockPrices = get_stock()
+    return render(request, 'stockPrices.html', {'stock': stockPrices})
