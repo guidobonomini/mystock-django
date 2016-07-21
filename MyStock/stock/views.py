41ab752a60fdb376f536e75f5b8875b7ec9bc891
Guido <bono_9475@hotmail.com>
2016-07-21 16:18:21 -0300

    Update views.py

--- MyStock/stock/views.py
+++ MyStock/stock/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from stock.services import get_stock
+from stock.services import *
+from stock.charts import *
 
 def index(request):
     
     stockPrices = get_stock()
     return render(request, 'stockPrices.html', {'stock': stockPrices})
+
+def stockDetail(request, symbol):
+    
+    historicalPrice = get_historicalPrice(symbol)
+    loadHistoricalChart(historicalPrice)
+    
+    return render(request, 'stockDetail.html')
