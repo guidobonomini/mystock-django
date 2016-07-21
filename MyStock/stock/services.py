29de0513fa1cf9fad68fe6815b9355ac7f642b19
Guido <bono_9475@hotmail.com>
2016-07-21 16:19:12 -0300

    Update services.py

--- MyStock/stock/services.py
+++ MyStock/stock/services.py
@@ -3,8 +3,16 @@ import json
 
 def get_stock():
     
-    url = 'http://marketdata.websol.barchart.com/getQuote.json?key=46447333b150fc404ba865c412fe70e0&symbols=IBM,GOOGL,APPL,PFE,AMZN,MSFT,INTC,EBAY,FB,YHOO,CSCO'
+    url = 'http://marketdata.websol.barchart.com/getQuote.json?key=46447333b150fc404ba865c412fe70e0&symbols=IBM,GOOGL,AAPL,PFE,AMZN,MSFT,INTC,EBAY,FB,YHOO,CSCO'
 
     result = requests.get(url)
     stockPrices = json.loads(result.text)
     return stockPrices
+
+
+def get_historicalPrice(symbol):
+
+    url = 'http://marketdata.websol.barchart.com/getHistory.json?key=46447333b150fc404ba865c412fe70e0&symbol=' + symbol +'&type=weekly&startDate=20150714000000'
+    result = requests.get(url)
+    historicalPrice = json.loads(result.text)
+    return historicalPrice
